713495193f1b448dbfd669443370bb043f04919d
shivam190103 <shivam2213010@akgec.ac.in>
2025-07-02 18:54:14 +0530

    Add 0_3conversionOpration.js

---
+++ 0_3conversionOpration.js
@@ -0,0 +1,42 @@
+let score="33" 
+
+console.log(typeof(score));//string 
+ 
+let valueInNumber=Number(score) //use data type in capital leters
+//type cast values
+console.log(typeof valueInNumber)
+
+
+//case 2:
+let Score="33abc" //it is not a number 
+
+console.log(typeof(score));//string 
+ 
+let ValueInNumber=Number(Score) //use data type in capital leters
+//but when type cast values then converted into the number but how 33abc can be number
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber);// NaN not a number hence take care of this 
+//for undefined data type  conversion gives the NaN
+//for null it gives 0 
+//if string cannot be converted into number it give NaN
+//null give 0
+//but NAN type is number
+
+
+//case-3 
+let isloggedIn=1;//number
+let booleanisLoggedIn=Boolean(isloggedIn)
+console.log(booleanisLoggedIn)//true
+//1 ==> true;
+//0 ==> false;
+//""==> false ;
+//"anything"==> true, even whitespace will give true
+
+
+
+//case-4 
+let someNumber=33//number 
+
+let stringNumber= String(someNumber)//typecast into the string
+console.log(stringNumber);//33
+console.log(typeof(stringNumber))//string
